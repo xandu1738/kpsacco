9225b0f92e3ce1298822886e89dc8c01b004f3ff
xandu1738 <51375353+xandu1738@users.noreply.github.com>
2022-12-15 23:13:30 +0300

    sku

--- kpsacco/settings.py
+++ kpsacco/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v66_zijvwj=-h7o4$n+ov%cr)9i_gyv5vmzy+s$_bza54)m$%s'
+SECRET_KEY = 'xxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
